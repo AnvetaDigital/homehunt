0fecfca2e2f9d0d8f0c0d5e8f1a7a2d7738df7ed
AM-LP-29 <nangarea2017@gmail.com>
2026-04-23 10:43:23 +0530

    [fix]: import Link for redirecting users to properties route by Browse Propertise link and add styling to page.tsx

--- homehunt/src/app/page.tsx
+++ homehunt/src/app/page.tsx
@@ -1,10 +1,17 @@
 import AuthButton from "@/components/AuthButton";
+import Link from "next/link";
 
 export default function Home() {
   return (
-      <main>
-        <h1>HomeHunt</h1>
-        <AuthButton/>
-      </main>
+    <main className="flex flex-col items-center justify-center h-screen gap-6">
+      <h1 className="text-3xl font-bold">HomeHunt</h1>
+      <AuthButton/>
+
+      <Link href="/properties">
+        <button className="bg-black text-white px-6 py-2 rounded">
+          Browse Properties
+        </button>
+      </Link>
+    </main>
   );
 }
