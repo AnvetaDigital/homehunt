3ddfa4c1febabfe85d11888aeb238eceb4532a0b
AM-LP-29 <nangarea2017@gmail.com>
2026-04-27 17:03:25 +0530

    [fix]: add AuthButton component in layout.tsx

--- homehunt/src/app/layout.tsx
+++ homehunt/src/app/layout.tsx
@@ -1,17 +1,24 @@
 import "./globals.css";
 import Providers from "@/components/Providers";
+import AuthButton from "@/components/AuthButton";
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html 
-          lang="en"
-          >
-      <body className="min-h-screen flex flex-col">
-        <Providers>{children}</Providers>
+    <html lang="en">
+      <body>
+        <Providers>
+
+          <div className="fixed top-4 right-6 z-50">
+            <AuthButton />
+          </div>
+
+          {children}
+
+        </Providers>
       </body>
     </html>
   );
