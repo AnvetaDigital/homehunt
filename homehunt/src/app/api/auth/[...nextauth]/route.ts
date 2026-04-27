162484a152fc73c0bbd6d0337341e292dafabea4
AM-LP-29 <nangarea2017@gmail.com>
2026-04-27 17:14:00 +0530

    [fix]: add authoptions in handler route

--- homehunt/src/app/api/auth/[...nextauth]/route.ts
+++ homehunt/src/app/api/auth/[...nextauth]/route.ts
@@ -1,49 +1,7 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { connectDB } from "@/lib/db";
-import User from "@/models/User";
+import { authOptions } from "@/lib/auth";
 
-const handler = NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-        }),
-    ],
-    callbacks: {
-    async signIn({ user }) {
-      await connectDB();
-
-      let existingUser = await User.findOne({ email: user.email });
-
-      if (!existingUser) {
-        existingUser = await User.create({
-          email: user.email,
-          name: user.name,
-          image: user.image,
-        });
-      }
-
-      user.id = existingUser._id.toString();
-      return true;
-    },
-
-    async jwt({ token, user }) {
-    if (user) {
-      token.id = user.id; // store in JWT
-    }
-    return token;
-  },
-
-    async session({ session, token }) {
-    if (session.user) {
-      session.user.id = token.id as string; // read from JWT and add to session
-    }
-    return session;
-  },
-
-}
-})
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
 
