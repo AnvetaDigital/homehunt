a208b709707f74d22909bad1383ca91bf1e8c4fa
AM-LP-29 <nangarea2017@gmail.com>
2026-04-28 15:35:27 +0530

    import z for validating data, authOptions, getServerSession, mongoose, cloudinary, getCoordinates, add PUT & DELETE apis for properties

--- homehunt/src/app/api/properties/[id]/route.ts
+++ homehunt/src/app/api/properties/[id]/route.ts
@@ -1,7 +1,16 @@
 import { NextResponse } from "next/server";
 import { connectDB } from "@/lib/db";
 import Property from "@/models/Property";
+import z from "zod";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import mongoose from "mongoose";
+import cloudinary from "@/lib/cloudinary";
+import { getCoordinates } from "@/lib/geocode";
 
+// =====================
+// GET
+// =====================
 export async function GET(
   req: Request,
   context: { params: Promise<{ id: string }> }
@@ -23,6 +32,163 @@ export async function GET(
     return NextResponse.json(
       { error: "Failed to fetch property" },
       { status: 500 }
+    );
+  }
+}
+
+// =====================
+// PUT
+// =====================
+const UpdateSchema = z.object({
+  title: z.string().min(3).optional(),
+  description: z.string().min(10).optional(),
+  price: z.number().positive().optional(),
+  category: z.enum(["apartment", "villa", "plot", "commercial"]).optional(),
+  location: z
+    .object({
+      city: z.string().min(2),
+    })
+    .optional(),
+  images: z
+    .array(
+      z.object({
+        url: z.string(),
+        public_id: z.string(),
+      }),
+    )
+    .optional(),
+});
+
+export async function PUT(
+  req: Request,
+  context: { params: Promise<{ id: string }> },
+) {
+  try {
+    await connectDB();
+
+    const session = await getServerSession(authOptions);
+
+    if (!session?.user?.id) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const { id: propertyId } = await context.params;
+
+    if (!mongoose.Types.ObjectId.isValid(propertyId)) {
+      return NextResponse.json(
+        { error: "Invalid property ID" },
+        { status: 400 },
+      );
+    }
+
+    const property = await Property.findById(propertyId);
+
+    if (!property) {
+      return NextResponse.json(
+        { error: "Property not found" },
+        { status: 404 },
+      );
+    }
+
+    //Ownership check
+    if (property.listedBy.toString() !== session.user.id) {
+      return NextResponse.json({ error: "Forbidden" }, { status: 403 });
+    }
+
+    const body = await req.json();
+
+    const parsed = UpdateSchema.safeParse(body);
+
+    if (!parsed.success) {
+      return NextResponse.json({ error: "Validation failed" }, { status: 400 });
+    }
+
+    let updateData: any = { ...parsed.data };
+
+    //Handle city update
+    if (parsed.data.location?.city) {
+      const coords = await getCoordinates(parsed.data.location.city);
+
+      if (!coords) {
+        return NextResponse.json({ error: "Invalid city" }, { status: 400 });
+      }
+
+      updateData.location = {
+        city: parsed.data.location.city,
+        coordinates: coords,
+      };
+    }
+
+    const updatedProperty = await Property.findByIdAndUpdate(
+      propertyId,
+      updateData,
+      { returnDocument: "after", runValidators: true },
+    );
+
+    return NextResponse.json(updatedProperty);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to update property" },
+      { status: 500 },
+    );
+  }
+}
+
+// =====================
+// DELETE
+// =====================
+export async function DELETE(
+  req: Request,
+  context: { params: Promise<{ id: string }> },
+) {
+  try {
+    await connectDB();
+
+    const session = await getServerSession(authOptions);
+
+    if (!session?.user?.id) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const { id: propertyId } = await context.params;
+
+    if (!mongoose.Types.ObjectId.isValid(propertyId)) {
+      return NextResponse.json(
+        { error: "Invalid property ID" },
+        { status: 400 },
+      );
+    }
+
+    const property = await Property.findById(propertyId);
+
+    if (!property) {
+      return NextResponse.json(
+        { error: "Property not found" },
+        { status: 404 },
+      );
+    }
+
+    // Ownership check
+    if (property.listedBy.toString() !== session.user.id) {
+      return NextResponse.json({ error: "Forbidden" }, { status: 403 });
+    }
+
+    // Delete images from Cloudinary
+    for (let img of property.images) {
+      try {
+        await cloudinary.uploader.destroy(img.public_id);
+      } catch (err) {
+        console.error("Failed to delete image:", img.public_id);
+      }
+    }
+
+    await Property.findByIdAndDelete(propertyId);
+
+    return NextResponse.json({ message: "Property deleted" });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to delete property" },
+      { status: 500 },
     );
   }
 }
