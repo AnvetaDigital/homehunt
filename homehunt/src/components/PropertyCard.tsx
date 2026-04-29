6737b3dde91dbc5e309bbbe7747854efebc827b2
AM-LP-29 <nangarea2017@gmail.com>
2026-04-29 15:37:29 +0530

    [fix]: add buttons in property card

--- homehunt/src/components/PropertyCard.tsx
+++ homehunt/src/components/PropertyCard.tsx
@@ -1,8 +1,42 @@
 "use client";
 
 import Link from "next/link";
+import { useRouter } from "next/navigation"
+import { useTransition } from "react";
 
 export default function PropertyCard({ property, onShowMap }: any) {
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+
+  const handleDelete = async () => {
+    const confirmDelete = confirm("Delete this property?");
+
+    if(!confirmDelete) return;
+
+    try{
+      const res = await fetch(`/api/properties/${property._id}`, {
+        method: "DELETE",
+      });
+
+      const data = await res.json();
+
+      if(!res.ok){
+        alert(data.error);
+        return;
+      }
+
+      alert("Deleted successfully");
+
+      //Refresh server data
+      startTransition(()=>{
+        router.refresh();
+      });
+    }catch(err){
+      console.error(err);
+      alert("Delete failed");
+    }
+  };
+
   return (
     <div className="border rounded-lg p-4 shadow">
 
@@ -24,8 +58,9 @@ export default function PropertyCard({ property, onShowMap }: any) {
         ₹ {property.price.toLocaleString("en-IN")}
       </p>
 
-      <div className="flex gap-2 mt-4">
+      <div className="flex gap-3 mt-4">
 
+        {/* Action Buttons */}
         <Link href={`/properties/${property._id}`}>
           <button className="bg-blue-600 text-white px-3 py-1 rounded">
             See Details
@@ -39,6 +74,22 @@ export default function PropertyCard({ property, onShowMap }: any) {
           Show on Map
         </button>
 
+        <button
+        className="bg-blue-500 text-white px-3 py-1 rounded"
+        onClick={()=>
+          router.push(`/properties/edit/${property._id}`)
+        }
+        >
+          Edit
+        </button>
+
+        <button
+        onClick={handleDelete}
+        disabled={isPending}
+        className="bg-red-500 text-white px-3 py-1 rounded"
+        >
+          {isPending ? "Deleting..." : "Delete"}
+        </button>
       </div>
     </div>
   )
