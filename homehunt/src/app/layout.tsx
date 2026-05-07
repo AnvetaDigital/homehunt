bdd3b4949dc83c1b5fcdb4250f66ac31e5cf9ea5
AM-LP-29 <nangarea2017@gmail.com>
2026-05-07 10:55:51 +0530

    [fix]: import FavoritesProvider in layout & apply styles to the Toaster

--- homehunt/src/app/layout.tsx
+++ homehunt/src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import Providers from "@/components/Providers";
 import AuthButton from "@/components/AuthButton";
 import { Toaster } from "react-hot-toast";
+import { FavoritesProvider } from "@/context/FavoritesContext";
 
 export default function RootLayout({
   children,
@@ -12,13 +13,21 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Providers>
-
           <div className="fixed top-4 right-6 z-50">
             <AuthButton />
           </div>
-
-          {children}
-          <Toaster position="top-right" />
+          <FavoritesProvider>{children}</FavoritesProvider>
+          <Toaster
+            position="top-right"
+            toastOptions={{
+              duration: 3000,
+              style: {
+                borderRadius: "8px",
+                background: "#333",
+                color: "#fff",
+              },
+            }}
+          />
         </Providers>
       </body>
     </html>
