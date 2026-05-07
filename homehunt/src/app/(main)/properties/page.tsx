59a1c8c815f37083f4f7e6c91ff2b48f5e911e1b
AM-LP-29 <nangarea2017@gmail.com>
2026-05-07 10:53:48 +0530

    [fix]: remove unwanted comment

--- homehunt/src/app/(main)/properties/page.tsx
+++ homehunt/src/app/(main)/properties/page.tsx
@@ -24,7 +24,6 @@ export default async function PropertiesPage({ searchParams }: Props) {
 
       <Filters />
 
-      {/* Map + List handled together */}
       <MapWrapper properties={data?.data || []} />
 
       <Pagination
