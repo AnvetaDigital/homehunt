27d92166fedbc7a93fa061a9d3e9ddb545d786df
AM-LP-29 <nangarea2017@gmail.com>
2026-05-07 10:47:24 +0530

    [fix]: remove unwanted code from PropertyList component

--- homehunt/src/components/PropertyList.tsx
+++ homehunt/src/components/PropertyList.tsx
@@ -4,7 +4,6 @@ import PropertyCard from "./PropertyCard";
 
 export default function PropertyList({
   properties,
-  selectedId,
   onShowMap
 }: any) {
   if (!properties || properties.length === 0) {
@@ -13,23 +12,16 @@ export default function PropertyList({
 
   return (
     <div className="grid grid-cols-1 gap-4">
-      {properties.map((property: any) => (
-        <div
-          id={`property-${property._id}`}
-          key={property._id}
-          className={`cursor-pointer rounded p-2 border ${
-            selectedId === property._id
-              ? "border-blue-500 shadow-lg"
-              : "border-gray-200"
-          }`}
-        >
+      {properties.map((property: any) => {
+
+        return (
           <PropertyCard
-          key={property._id}
-          property={property} 
-          onShowMap={onShowMap}
+            key={property._id}
+            property={property}
+            onShowMap={onShowMap}
           />
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 }
